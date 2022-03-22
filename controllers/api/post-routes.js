b2ead1c5356e514447440b60ed8e942b57ef0eb8
camilleNicole <camilleht@gmail.com>
2022-03-21 23:38:38 -0400

    comments route added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
-// get all users
+// get all posts
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: ['id', 'post_url',
@@ -11,7 +11,16 @@ router.get('/', (req, res) => {
          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
         include: [
-            {
+          // include the Comment model here:
+   {
+    model: Comment,
+    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    include: {
+      model: User,
+      attributes: ['username']
+    }
+  },
+      {
               model: User,
               attributes: ['username']
             }
@@ -52,6 +61,7 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+  
 router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
@@ -68,29 +78,9 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-   // create the vote
-Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  }).then(() => {
-    // then find the post we just voted on
-    return Post.findOne({
-      where: {
-        id: req.body.post_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-    .then(dbPostData => res.json(dbPostData))
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
@@ -120,7 +110,7 @@ router.put('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
-});
+
 
 router.delete('/:id', (req, res) => {
     Post.destroy({
--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require('./Votes');
+const Comment = require('./Comment');
 
-
-// create associations
+//table associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
   });
@@ -40,4 +40,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
